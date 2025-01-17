9543bb974f05fcca7ef585e2243fb8a46fddceef
Taylor Hardy <hardy.taylor.j@gmail.com>
2025-01-17 17:27:47 -0500

    use other english translation

--- dist/translate.bash
+++ dist/translate.bash
@@ -1,4 +1,9 @@
+# for filename in ./*.png.txt; do
+#     echo "translating $filename"
+#     deepl -t en -o $(basename "$filename" .txt).eng.txt $filename
+# done
 for filename in ./*.png.txt; do
     echo "translating $filename"
-    deepl -t en -o $(basename "$filename" .txt).eng.txt $filename
+    trans ja:en --brief -i $filename -o $(basename "$filename" .txt).eng2.txt
+
 done
